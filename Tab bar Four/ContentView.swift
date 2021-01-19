f469d4309d1e1b55c5e8b9f811f022da6a128f14
hosam <hosamelmalt@gmail.com>
2021-01-19 08:14:45 +0200

    Initial Commit

---
+++ Tab bar Four/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tab bar Four
+//
+//  Created by hosam on 1/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tab bar Four/Tab_bar_FourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_bar_FourApp.swift
+//  Tab bar Four
+//
+//  Created by hosam on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Tab_bar_FourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
